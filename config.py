2a8ecab916d48dec99d3dea4e12f09253c3ff4ab
Abhinav kumar <111506275+Probou8272@users.noreply.github.com>
2023-02-27 00:23:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "˹ɪsʜᴀ ✘ ᴛᴄʜ˼")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "˹ɪsʜᴀ ✘ ᴍᴜsɪᴄ˼")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6095755800").split()))
 
