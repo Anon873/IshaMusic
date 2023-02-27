00d22218bc42892766bdff3efa8fa12c900bc5fa
Abhinav kumar <111506275+Probou8272@users.noreply.github.com>
2023-02-27 14:28:06 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -35,15 +35,31 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ],
         [
+          
             InlineKeyboardButton(
-                text="• ʜᴇʟᴩ •", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=config.SUPPORT_GROUP
+
+                text="˹sᴜᴩᴩᴏʀᴛ˼", url=config.SUPPORT_GROUP
+
             ),
+
             InlineKeyboardButton(
+
+                text="˹ᴜᴘᴅᴀᴛᴇs˼", url=config.SUPPORT_CHANNEL
+
+            )
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text="♡ ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs ♡", callback_data="settings_back_helper"
+                )
+
+        ],
+
+        [
                 text="🥀 ᴅᴇᴠᴇʟᴏᴘᴇʀ 🥀 ", user_id=OWNER
             )
         ],
